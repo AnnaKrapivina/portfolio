d345adf323d4b3916afe7cd7051d4610c22c6c89
Annie <anna-krapivina@mail.ru>
2017-07-09 01:19:51 +0300

    edit js

--- landing2/js/app.js
+++ landing2/js/app.js
@@ -34,7 +34,7 @@ $(function() {
         event.preventDefault();
         var $id  = $(this).attr('href');
         var $top = $($id).offset().top;
-        $('body,html').animate({scrollTop: $top-60}, 1500);
+        $('body,html').animate({scrollTop: $top-80}, 300);
 
     });
 
